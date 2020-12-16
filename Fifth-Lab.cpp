03c3c5c405b69ac2f105546a4c9036d1b3e47eed
Tobalanty <gg1gg141@gmail.com>
2020-12-17 00:54:58 +0300

    .

--- Fifth-Lab.cpp
+++ Fifth-Lab.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include "cinClear.h" //Для проверки ввода с клавиатуры
 
 /*1 Вариант*/
 /*Задана матрица размером NxM. Получить массив B, присвоив его k-му
@@ -6,30 +7,46 @@
 и значение 1 − в противном случае*/
 /*int main()
 {
-	std::cout << "Input N: ";
-	int n;
-	std::cin >> n;
-	std::cout << "Input M: ";
-	int m;
-	std::cin >> m;
+	int n;								//Ввод количества строк			
+	while (true) {
+		std::cout << "Input N: ";
+		std::cin >> n;
+		if (cinClear()) {				//Проверка ввода
+			break;
+		}
+	}
 
-	int **array = new int*[n];
+	int m;								//Ввод количества столбцов
+	while (true) {
+		std::cout << "Input M: ";
+		std::cin >> m;
+		if (cinClear()) {
+			break;
+		}
+	}
+
+	int **array = new int*[n];			//Объявление двумерного динамического массива
 
 	for (int i = 0; i < n; ++i) {
 		array[i] = new int[m];
 	}
 
-	int* arrayB = new int[m] {};
+	int* arrayB = new int[m] {};	//Объявление массива, хранящего результат проверки матрицы
 
-	std::cout << "fill the array with integers.\n";
+	std::cout << "fill the array with integers.\n";	
 	
-	for (int i = 0;i < n; ++i) {
+	for (int i = 0;i < n; ++i) {				//Заполнение матрицы с клавиатуры
 		for (int j = 0; j < m; ++j) {
-			std::cin >> array[i][j];
+			while (true) {
+				std::cin >> array[i][j];
+				if (cinClear()) {
+					break;
+				}
+			}
 		}
 	}
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i) {					//Вывод матрицы в понятном виде
 		for (int j = 0; j < m; ++j) {
 			std::cout << array[i][j] << " ";
 		}
@@ -37,7 +54,7 @@
 	}
 
 
-	for (int j = 0; j < n; ++j) {
+	for (int j = 0; j < n; ++j) {				//Проверка матрицы соглсано условию
 		for (int i = 0; i < m; ++i) {
 			if (array[i][j] != 0) {
 				arrayB[j] = 1;
@@ -46,11 +63,16 @@
 		}
 	}
 
-	std::cout << "arrayB now:\n";
+	std::cout << "arrayB now:\n";				//Вывод массива-результата
 	for (int j = 0; j < m; ++j) {
 		std::cout << arrayB[j] << "\n";
 	}
 
+	for(int i = 0; i < n; ++i){				//Освобождение памяти
+	delete []array[i];
+	}
+	delete []array;
+	delete []arrayB;
 
 	system("pause");
 	return 0;
@@ -61,30 +83,46 @@
 и значение 0 − в противном случае*/
 /*int main() {
 
-	std::cout << "Input N: ";
-	int n;
-	std::cin >> n;
-	std::cout << "Input M: ";
-	int m;
-	std::cin >> m;
+	int n;								//Ввод количества строк			
+	while (true) {
+		std::cout << "Input N: ";
+		std::cin >> n;
+		if (cinClear()) {				//Проверка ввода
+			break;
+		}
+	}
 
-	int** array = new int* [n];
+	int m;								//Ввод количества столбцов
+	while (true) {
+		std::cout << "Input M: ";
+		std::cin >> m;
+		if (cinClear()) {
+			break;
+		}
+	}
+
+	int** array = new int* [n];				//Объявление думмерного динамического массива (матрицы)
 
 	for (int i = 0; i < n; ++i) {
 		array[i] = new int[m];
 	}
 
-	int *arrayB = new int[n] {};
+	int *arrayB = new int[n] {};			//Массив, хранящий результат проверки на упорядоченность строк
 
-	std::cout << "fill the array with integers.\n";
+	std::cout << "fill the array with integers.\n";		//Заполнение матрицы с клавиатуры
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
-			std::cin >> array[i][j];
+			while (true) {
+				std::cin >> array[i][j];
+				if (cinClear()) {
+					break;
+				}
+			}
 		}
 	}
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i) {					//Проверка на упорядоченность
 
 		bool k{true};
 
@@ -99,12 +137,12 @@
 		}
 	}
 
-	std::cout << "arrayB now:\n";
+	std::cout << "arrayB now:\n";					//Вывод массива-результата
 	for (int i = 0; i < n; ++i) {
 		std::cout << arrayB[i] << "\n";
 	}
 
-	for(int i = 0; i < n; ++i){
+	for(int i = 0; i < n; ++i){				//Освобождение памяти
 	delete []array[i];
 	}
 	delete []array;
@@ -113,49 +151,64 @@
 	system("pause");
 	return 0;
 }*/
-
 /*3 Вариант*/
 /*Задана матрица размером NxM. Получить массив B, присвоив его k-му
 элементу значение 1, если k-я строка матрицы симметрична, и значение 0 − в
 противном случае*/
 /*int main() {
 
-	std::cout << "Input N: ";
-	int n;
-	std::cin >> n;
-	std::cout << "Input M: ";
-	int m;
-	std::cin >> m;
+	int n;								//Ввод количества строк			
+	while (true) {
+		std::cout << "Input N: ";
+		std::cin >> n;
+		if (cinClear()) {				//Проверка ввода
+			break;
+		}
+	}
 
-	int **array = new int*[n];
+	int m;								//Ввод количества столбцов
+	while (true) {
+		std::cout << "Input M: ";
+		std::cin >> m;
+		if (cinClear()) {
+			break;
+		}
+	}
 
+	int **array = new int*[n];			//Объявление двумерного динамического массива (матрицы)
 	for (int i = 0; i < n; ++i) {
 		array[i] = new int[m];
 	}
 
-	std::cout << "Fill the array with integer: ";
+	std::cout << "Fill the array with integer: ";		
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i) {		//Заполнение матрицы пользователем
 		for (int j = 0; j < m; ++j) {
-			std::cin >> array[i][j];
+			while (true) {
+				std::cin >> array[i][j];
+				if (cinClear()) {
+					break;
+				}
+			}
 		}
 	}
 
-	std::cout << "Your array: \n";
+	std::cout << "Your array: \n";				//Вывод матрицы в понятном виде
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i) {				
 		for (int j = 0; j < m; ++j) {
 			std::cout << array[i][j] << " ";
 		}
 		std::cout << "\n";
 	}
 
-	bool* arrayB = new bool[n]{};
+	bool* arrayB = new bool[n]{};			//Обявление одномерного динамического массива, хранящего результат проверки на симметрию строк матрицы array
+
 	for (int i = 0; i < n; ++i) {
 
 		bool k = true;
 
-		for (int j = 0; j < m/2; ++j) {
+		for (int j = 0; j < m/2; ++j) {					//Сама проверка
 			if (array[i][m - j - 1] != array[i][j]) {
 				k = false;
 				break;
@@ -166,12 +219,12 @@
 		}
 	}
 	
-	std::cout << "arrayB now:\n";
+	std::cout << "arrayB now:\n";				//Вывод массива-результата проверки на симметричность
 	for (int i = 0; i < n; ++i) {
 		std::cout << arrayB[i] << " ";
 	}
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i) {			//Освобождение памяти
 		delete[]array[i];
 	}
 	delete[]array;
@@ -180,17 +233,136 @@
 	system("pause");
 	return 0;
 }*/
-
 /*4 Вариант*/
 /* Задана матрица размером NxM. Определить количество «особых» элементов матрицы, 
 считая элемент «особым», если он больше суммы остальных
 элементов своего столбца*/
+/*int main() {
 
+	int n;								//Ввод количества строк			
+	while (true) {
+		std::cout << "Input N: ";
+		std::cin >> n;
+		if (cinClear()) {				//Проверка ввода
+			break;
+		}
+	}
+
+	int m;								//Ввод количества столбцов
+	while (true) {
+		std::cout << "Input M: ";
+		std::cin >> m;
+		if (cinClear()) {
+			break;
+		}
+	}
+
+	int **array = new int *[n];			//Объявление двумерного динамического массива 
+	for (int i = 0; i < n; ++i) {
+		array[i] = new int[m];
+	}
+
+	std::cout << "Fill the matrix with integers: \n";	//Заполнение полученной матрицы значениями введёнными пользователем
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			while (true) {
+				std::cin >> array[i][j];
+				if (cinClear()) {
+					break;
+				}
+			}
+		}
+	}
+
+	int *sum = new int[m]{};				//Одномерный массив для хранения суммы каждого столбца матрицы
+
+	for (int j = 0; j < m; ++j) {			//Запись суммы каждого столбца матрицы в элемент под соответствующим номером массива sum
+		for (int i = 0; i < n; ++i) {
+			sum[j] += array[i][j];
+		}
+	}
+
+	int k{ 0 };								//Переменная, хранящая количество "особых" элементов
+
+	for (int j = 0; j < m; ++j) {						//Поиск "особых" элементов и увеличение счётчика k на 1 при нахождении такого
+		for (int i = 0; i < n; ++i) {
+			if (array[i][j] > (sum[j] - array[i][j])) {	
+				k++;
+			}
+		}
+	}
+
+	std::cout << "Your matrix is: \n";				//Вывод исходной матрицы в понятном виде
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			std::cout << array[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	delete []sum;							//Освобождение памяти
+	for (int i = 0; i < n; ++i) {
+		delete []array[i];
+	}
+	delete []array;
+
+	std::cout << "There are " << k << " 'special' elements in this matrix.\n";
+
+	system("pause");
+	return 0;
+}*/
 /*5 Вариант*/
 /* Задана матрица размером NxM. Определить количество «особых» элементов матрицы, 
 считая элемент «особым», если в строке слева от него находятся элементы, меньшие его, 
 а справа – большие*/
+int main() {
 
+	int n;								//Ввод количества строк			
+	while (true) {
+		std::cout << "Input N: ";
+		std::cin >> n;
+		if (cinClear()) {				//Проверка ввода
+			break;
+		}
+	}
+
+	int m;								//Ввод количества столбцов
+	while (true) {
+		std::cout << "Input M: ";
+		std::cin >> m;
+		if (cinClear()) {
+			break;
+		}
+	}
+
+	int** array = new int* [n];			//Объявление двумерного динамического массива 
+	for (int i = 0; i < n; ++i) {
+		array[i] = new int[m];
+	}
+
+	std::cout << "Fill the matrix with integers: \n";	//Заполнение полученной матрицы значениями введёнными пользователем
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			while (true) {
+				std::cin >> array[i][j];
+				if (cinClear()) {
+					break;
+				}
+			}
+		}
+	}
+
+	std::cout << "Your matrix is: \n";				//Вывод исходной матрицы в понятном виде
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			std::cout << array[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	
+	system("pause");
+	return 0;
+}
 /*6 Вариант*/
 /*Задана символьная матрица размером NxM.
 Определить количество различных элементов матрицы (т.е. повторяющиеся элементы считать один раз)*/
---
+++ cinClear.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#ifndef CINCLEAR
+#define CINCLEAR
+
+bool cinClear() {
+
+	if (std::cin.fail()) {
+		std::cin.clear();
+		std::cin.ignore(32767, '\n');
+		std::cout << "Whops, something went wrong. Please, try again.\n";
+		return false;
+	}
+
+	std::cin.ignore(3277, '\n');
+	return true;
+}
+
+#endif
